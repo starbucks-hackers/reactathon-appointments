a92c5b655175006d123b7fc60c2af346675bc7f1
Brian Muse <bmuse@pdiarm.com>
2017-03-12 11:50:53 -0700

    Initial deploy functionality.

---
+++ appointments/create.js
@@ -0,0 +1,46 @@
+'use strict';
+
+const AWS = require('aws-sdk'); // eslint-disable-line import/no-extraneous-dependencies
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+module.exports.create = (event, context, callback) => {
+  const timestamp = new Date().getTime();
+  const data = JSON.parse(event.body);
+  if (typeof data.text !== 'string') {
+    console.error('Validation Failed');
+    callback(new Error('Couldn\'t create the todo item.'));
+    return;
+  }
+
+  const params = {
+    TableName: process.env.DYNAMODB_TABLE,
+    Item: {
+      id: data.id,
+      userId: data.userId,
+      providerId: data.providerId,
+      day: data.day,
+      month: data.month,
+      time: data.time,
+      createdAt: timestamp,
+      updatedAt: timestamp,
+    },
+  };
+
+  // write the todo to the database
+  dynamoDb.put(params, (error, result) => {
+    // handle potential errors
+    if (error) {
+      console.error(error);
+      callback(new Error('Couldn\'t create the todo item.'));
+      return;
+    }
+
+    // create a response
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(result.Item),
+    };
+    callback(null, response);
+  });
+};
--- appointments/get.js
+++ appointments/get.js
@@ -5,6 +5,107 @@ const AWS = require('aws-sdk');
 
 const dynamoDb = AWS.DynamoDB.DocumentClient();
 
-module.exports.users = (event, context, callback) => {
+module.exports.get = (event, context, callback) => {
+  var appointmentId = event.pathParameters.appointmentId;
 
-}
+  const params = {
+    TableName: process.env.DYNAMODB_TABLE,
+    Key: {
+      id: appointmentId,
+    },
+  };
+
+  // fetch todo from the database
+  dynamoDb.get(params, (error, result) => {
+    // handle potential errors
+    if (error) {
+      consolse.error(error);
+      callback(new Error('Couldn\'t fetch the todo item.'));
+      return;
+    }
+
+    // create a response
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(result.Item),
+    };
+    callback(null, response);
+  });
+
+};
+
+module.exports.list = (event, context, callback) => {
+  const params = {
+    TableName: process.env.DYNAMODB_TABLE,
+  };
+  // fetch all todos from the database
+  dynamoDb.scan(params, (error, result) => {
+    // handle potential errors
+    if (error) {
+      console.error(error);
+      callback(new Error('Couldn\'t fetch the Providers.'));
+      return;
+    }
+
+    // create a response
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(result.Items),
+    };
+    callback(null, response);
+  });
+};
+
+module.exports.getProviderAppointments = (event, context, callback) => {
+  var providerId = event.pathParameters.providerId;
+
+  const params = {
+    TableName: process.env.DYNAMODB_TABLE,
+    ExpressionAttributeValues:{":providerId": providerId},
+    FilterExpression: "providerId = :providerId",
+  };
+
+  // fetch todo from the database
+  dynamoDb.get(params, (error, result) => {
+    // handle potential errors
+    if (error) {
+      consolse.error(error);
+      callback(new Error('Couldn\'t fetch the todo item.'));
+      return;
+    }
+
+    // create a response
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(result.Item),
+    };
+    callback(null, response);
+  });
+};
+module.exports.getUserAppointments = (event, context, callback) => {
+  var userId = event.pathParameters.userId;
+
+  const params = {
+    TableName: process.env.DYNAMODB_TABLE,
+    ExpressionAttributeValues:{":userId": userId},
+    FilterExpression: "userId = :userId",
+  };
+
+  // fetch todo from the database
+  dynamoDb.get(params, (error, result) => {
+    // handle potential errors
+    if (error) {
+      consolse.error(error);
+      callback(new Error('Couldn\'t fetch the todo item.'));
+      return;
+    }
+
+    // create a response
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(result.Item),
+    };
+    callback(null, response);
+  });
+};
+
