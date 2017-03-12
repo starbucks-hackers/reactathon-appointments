3f0c0a9eb407e398614752344c270ef055e51f65
Brian Muse <bmuse@pdiarm.com>
2017-03-11 19:10:26 -0800

    Updated serverless.yaml and added functionality.

---
+++ appointments/get.js
@@ -0,0 +1,10 @@
+'use strict'
+
+const uuid = require('uuid');
+const AWS = require('aws-sdk');
+
+const dynamoDb = AWS.DynamoDB.DocumentClient();
+
+module.exports.users = (event, context, callback) => {
+
+}
